00f2cda2e253280ecefdbfb881da97805ce62180
vendor281 <napatt.kuttikun@tcs.com>
2021-03-17 13:09:20 +0700

    ONEAPP-6798 Initialize Project

---
+++ src/main/java/com/tmb/oneapp/lendingservice/constant/LendingServiceConstant.java
@@ -0,0 +1,42 @@
+package com.tmb.oneapp.lendingservice.constant;
+
+
+/**
+ * Constant class for Application
+ *
+ */
+public class LendingServiceConstant {
+
+    /**
+     * created private constructor so that no body can create object of this call
+     */
+    private LendingServiceConstant() {
+    }
+
+    public static final String INITIALIZE_SSL_CONTEXT = "[initializeSSLContext] ";
+    public static final String EXCEPTION_OCCURED = "Exception occured : {}";
+
+    /**
+     * REQUEST HEADERS
+     */
+    public static final String HEADER_X_FORWARD_FOR = "x-forward-for";
+    public static final String HEADER_OS_VERSION = "os-version";
+    public static final String HEADER_CHANNEL = "channel";
+    public static final String HEADER_APP_VERSION = "app-version";
+    public static final String HEADER_X_CRMID = "x-crmid";
+    public static final String HEADER_DEVICE_ID = "device-id";
+    public static final String HEADER_DEVICE_MODEL = "device-model";
+
+    /**
+     * ACTIVITY IDS
+     */
+    public static final String FINISH_BLOCK_CARD_ACTIVITY_ID = "00700402";
+
+    /**
+     * ACTIVITY LOGGING FIELDS
+     */
+    public static final String ACTIVITY_SCREEN_NAME_TUTORIAL_CST = "tutorial case tracking";
+
+
+
+}

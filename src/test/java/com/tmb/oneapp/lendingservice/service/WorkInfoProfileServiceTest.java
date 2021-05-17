33f79d38318131c509d3b7281a5ef492c202cd77
Witsanu Thammayon <witsanu.t@tcs.com>
2021-05-17 18:49:47 +0700

    add test case supported

---
+++ src/test/java/com/tmb/oneapp/lendingservice/service/WorkInfoProfileServiceTest.java
@@ -0,0 +1,13 @@
+package com.tmb.oneapp.lendingservice.service;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+
+@RunWith(JUnit4.class)
+public class WorkInfoProfileServiceTest {
+	
+	@Mock
+	private WorkInfoProfileService workInfoProfileService;
+
+}

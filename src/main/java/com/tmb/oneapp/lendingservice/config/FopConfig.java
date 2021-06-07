603fd10d120eb2aa4bb863e482fbff5a0f8142ee
vendor245 <phanlop.p@tcs.com>
2021-06-08 06:51:10 +0700

    poc generate image

--- src/main/java/com/tmb/oneapp/lendingservice/config/FopConfig.java
+++ src/main/java/com/tmb/oneapp/lendingservice/config/FopConfig.java
@@ -14,26 +14,41 @@ import org.xml.sax.SAXException;
 import javax.xml.transform.TransformerFactory;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 
 @Configuration
 public class FopConfig implements ResourceLoaderAware {
 
     private ResourceLoader resourceLoader;
-
     @Bean
     public FopFactory getFopFactory() throws IOException, ConfigurationException, SAXException {
 
-        Resource configResource = this.resourceLoader.getResource("classpath:fop/fop-config.xml");
-        File fopConfigFile = configResource.getFile();
+        String[] files = new String[]{"DBOzoneX.ttf", "DBOzoneX-Bold.ttf", "DBOzoneX-Italic.ttf", "fop-config.xml"};
+        File f1 = new File("./fop2");
+        f1.mkdir();
+
+        for (String file : files) {
+            Resource configResource = this.resourceLoader.getResource("classpath:fop/" + file);
+
+            InputStream inputStream = configResource.getInputStream();
+
+            Files.copy(inputStream, new File("./fop2/" + file).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        File fopConfigFile = new File("./fop2/fop-config.xml");
         File baseFolder = new File(fopConfigFile.getParent());
 
         DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder();
+
         org.apache.fop.configuration.Configuration cfg = cfgBuilder.buildFromFile(fopConfigFile);
+
+
         FopFactoryBuilder builder = new FopFactoryBuilder(baseFolder.toURI()).setConfiguration(cfg);
         return builder.build();
     }
-
 
     @Bean
     public TransformerFactory getTransformerFactory() {

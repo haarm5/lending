0f1fe49fb1d079b367b2b84631e850ae13ca4b6f
vendor245 <phanlop.p@tcs.com>
2021-06-16 00:13:37 +0700

    poc sftp libs

--- src/main/java/com/tmb/oneapp/lendingservice/controller/SFTPController.java
+++ src/main/java/com/tmb/oneapp/lendingservice/controller/SFTPController.java
@@ -1,7 +1,13 @@
 package com.tmb.oneapp.lendingservice.controller;
 
 
+import com.jcraft.jsch.*;
+import com.tmb.common.logger.TMBLogger;
 import com.tmb.oneapp.lendingservice.client.SFTPClientImp;
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.SFTPClient;
+import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.xfer.FileSystemFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,17 +22,69 @@ import java.util.concurrent.Executors;
 @RequestMapping("/sftp")
 @RestController
 public class SFTPController {
+    private static final TMBLogger<SFTPController> logger = new TMBLogger<>(SFTPController.class);
 
     @Autowired
     private SFTPClientImp sftpClientImp;
 
     private ExecutorService executor = Executors.newSingleThreadExecutor();
 
+    private SSHClient setupSshj() throws IOException {
+        SSHClient client = new SSHClient();
+        client.addHostKeyVerifier(new PromiscuousVerifier());
+        client.connect(sftpClientImp.remoteHost);
+        client.authPassword(sftpClientImp.username, sftpClientImp.password);
+        return client;
+    }
+
+    private ChannelSftp setupJsch() throws JSchException {
+        JSch jsch = new JSch();
+        //jsch.setKnownHosts("/Users/john/.ssh/known_hosts");
+        Session jschSession = jsch.getSession(sftpClientImp.username, sftpClientImp.remoteHost);
+        jschSession.setPassword(sftpClientImp.password);
+        java.util.Properties config = new java.util.Properties();
+        config.put("StrictHostKeyChecking", "no");
+        jschSession.setConfig(config);
+        jschSession.connect();
+        return (ChannelSftp) jschSession.openChannel("sftp");
+    }
+
+    @PostMapping(value = "/jsch")
+    public ResponseEntity jsch() throws JSchException, SftpException {
+        logger.info("start jsch");
+        ChannelSftp channelSftp = setupJsch();
+        channelSftp.connect();
+        String baseDir = System.getProperty("user.dir");
+        String localFile = baseDir + File.separator + "fop" + File.separator + "01_210615152223_abc_00110.JPG";
+        String remoteDir = sftpClientImp.locPrimaryLocation + "/jsch/";
+        channelSftp.mkdir(remoteDir);
+        channelSftp.put(localFile, remoteDir + "01_210615152223_abc_00110.JPG");
+        channelSftp.exit();
+        logger.info("end jsch");
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(value = "/sshj")
+    public ResponseEntity sshj() throws IOException {
+        logger.info("start sshj");
+        String directoryPath = sftpClientImp.locPrimaryLocation + "/sshj/";
+        String baseDir = System.getProperty("user.dir");
+        SSHClient sshClient = setupSshj();
+        SFTPClient sftpClient = sshClient.newSFTPClient();
+        String localFile = baseDir + File.separator + "fop" + File.separator + "01_210615152223_abc_00110.JPG";
+        sftpClient.mkdir(directoryPath);
+        sftpClient.put(new FileSystemFile(localFile), directoryPath + "01_210615152223_abc_00110.JPG");
+        sftpClient.close();
+        sshClient.disconnect();
+        logger.info("end sshj");
+        return ResponseEntity.ok().build();
+    }
+
     @PostMapping(value = "/normal")
     public ResponseEntity normal() throws IOException {
         String directoryPath = "normal/12345";
         String baseDir = System.getProperty("user.dir");
-        sftpClientImp.storeFile(directoryPath, baseDir+File.separator+"fop"+File.separator+"01_210615152223_abc_00110.JPG");
+        sftpClientImp.storeFile(directoryPath, baseDir + File.separator + "fop" + File.separator + "01_210615152223_abc_00110.JPG");
         return ResponseEntity.ok().build();
     }
 
@@ -42,7 +100,7 @@ public class SFTPController {
             String directoryPath = "async/12346";
             String baseDir = System.getProperty("user.dir");
             try {
-                sftpClientImp.storeFile(directoryPath, baseDir+File.separator+"fop"+File.separator+"01_210615152223_abc_00110.JPG");
+                sftpClientImp.storeFile(directoryPath, baseDir + File.separator + "fop" + File.separator + "01_210615152223_abc_00110.JPG");
             } catch (IOException e) {
                 e.printStackTrace();
             }

c04b2ff152d2360738a56ef75a96f56aff1910ed
Nok <omerta@odds.team>
2021-07-30 15:55:37 +0700

    fixed code smell

--- src/main/java/com/tmb/oneapp/lendingservice/service/LoanSubmissionCreateApplicationService.java
+++ src/main/java/com/tmb/oneapp/lendingservice/service/LoanSubmissionCreateApplicationService.java
@@ -28,7 +28,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Objects;
 
 @Service

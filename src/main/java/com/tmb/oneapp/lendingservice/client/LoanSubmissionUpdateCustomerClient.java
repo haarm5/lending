eb4b2ca56456306b55347784fbb851952d5a86f7
kamonwan <kamonwan294@gmail.com>
2021-08-03 17:04:16 +0700

    handle error soap

--- src/main/java/com/tmb/oneapp/lendingservice/client/LoanSubmissionUpdateCustomerClient.java
+++ src/main/java/com/tmb/oneapp/lendingservice/client/LoanSubmissionUpdateCustomerClient.java
@@ -1,5 +1,8 @@
 package com.tmb.oneapp.lendingservice.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tmb.common.logger.TMBLogger;
 import com.tmb.common.model.legacy.rsl.common.ob.individual.Individual;
 import com.tmb.common.model.legacy.rsl.ws.individual.update.request.Body;
 import com.tmb.common.model.legacy.rsl.ws.individual.update.request.Header;
@@ -18,19 +21,24 @@ import java.util.UUID;
 public class LoanSubmissionUpdateCustomerClient {
     @Value("${loan-submission-update-customer-info.url}")
     private String updateCustomerInfo;
+    private final ObjectMapper mapper;
+    private static final TMBLogger<LoanSubmissionUpdateCustomerClient> logger = new TMBLogger<>(LoanSubmissionUpdateCustomerClient.class);
 
     LoanSubmissionUpdateCustomerServiceLocator locator = new LoanSubmissionUpdateCustomerServiceLocator();
 
     private static final String CHANNEL = "MIB";
     private static final String MODULE = "3";
 
-    private void setLocator(LoanSubmissionUpdateCustomerServiceLocator locator) {
+    public LoanSubmissionUpdateCustomerClient(ObjectMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    public void setLocator(LoanSubmissionUpdateCustomerServiceLocator locator) {
         this.locator = locator;
     }
 
-    public ResponseIndividual updateCustomerInfo(Individual individual) throws RemoteException, ServiceException {
+    public ResponseIndividual updateCustomerInfo(Individual individual) throws RemoteException, ServiceException, JsonProcessingException {
         locator.setLoanSubmissionUpdateCustomerEndpointAddress(updateCustomerInfo);
-
         LoanSubmissionUpdateCustomerSoapBindingStub stub = (LoanSubmissionUpdateCustomerSoapBindingStub) locator.getLoanSubmissionUpdateCustomer();
 
         RequestIndividual req = new RequestIndividual();
@@ -43,8 +51,11 @@ public class LoanSubmissionUpdateCustomerClient {
 
         Body body = new Body();
         body.setIndividual(individual);
+        logger.info("Request from Client to updateCustomer is {} : " + mapper.writeValueAsString(req));
 
-        return stub.updateCustomerInfo(req);
+        ResponseIndividual responseIndividual = stub.updateCustomerInfo(req);
+        logger.info("Response from Client to updateCustomer is {} : " + mapper.writeValueAsString(responseIndividual));
+        return responseIndividual;
 
 
     }

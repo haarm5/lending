d3d9a8ba50f109f79d95e1abde4166791db44cc5
Oranuch <oranuch@odds.team>
2021-08-06 15:42:25 +0700

    create personal model

---
+++ src/main/java/com/tmb/oneapp/lendingservice/model/personal/PersonalDetailModel.java
@@ -0,0 +1,17 @@
+package com.tmb.oneapp.lendingservice.model.personal;
+
+import java.util.Calendar;
+
+public class PersonalDetailModel {
+    private String engName;
+    private String engSurName;
+    private String thaiName;
+    private String  thaiSurName;
+    private String email;
+    private Calendar birthDate;
+    private String idIssueCtry1;
+    private Calendar expiryDate;
+    private String nationality;
+    private Address address;
+    private String mobileNo;
+}

2d7630d4bb8a0034cb0ecde0eac49f151bba32f7
kamonwan <kamonwan294@gmail.com>
2021-08-16 22:05:34 +0700

    update response

--- src/main/java/com/tmb/oneapp/lendingservice/service/PersonalDetailSaveInfoService.java
+++ src/main/java/com/tmb/oneapp/lendingservice/service/PersonalDetailSaveInfoService.java
@@ -3,24 +3,24 @@ package com.tmb.oneapp.lendingservice.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tmb.common.exception.model.TMBCommonException;
 import com.tmb.common.logger.TMBLogger;
+import com.tmb.common.model.legacy.rsl.common.ob.dropdown.CommonCodeEntry;
 import com.tmb.common.model.legacy.rsl.common.ob.individual.Individual;
+import com.tmb.common.model.legacy.rsl.ws.dropdown.response.ResponseDropdown;
 import com.tmb.common.model.legacy.rsl.ws.individual.update.request.Body;
 import com.tmb.common.model.legacy.rsl.ws.individual.update.request.RequestIndividual;
 import com.tmb.common.model.legacy.rsl.ws.individual.update.response.ResponseIndividual;
 import com.tmb.oneapp.lendingservice.client.LoanSubmissionGetCustomerInfoClient;
+import com.tmb.oneapp.lendingservice.client.LoanSubmissionGetDropdownListClient;
 import com.tmb.oneapp.lendingservice.client.LoanSubmissionUpdateCustomerClient;
 import com.tmb.oneapp.lendingservice.constant.ResponseCode;
-import com.tmb.oneapp.lendingservice.model.personal.Address;
-import com.tmb.oneapp.lendingservice.model.personal.PersonalDetailSaveInfoRequest;
+import com.tmb.oneapp.lendingservice.model.personal.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.xml.rpc.ServiceException;
 import java.rmi.RemoteException;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @AllArgsConstructor
@@ -28,15 +28,18 @@ public class PersonalDetailSaveInfoService {
     private static final TMBLogger<PersonalDetailSaveInfoService> logger = new TMBLogger<>(PersonalDetailSaveInfoService.class);
     private final LoanSubmissionUpdateCustomerClient updateCustomerClient;
     private final LoanSubmissionGetCustomerInfoClient getCustomerInfoClient;
+    private final LoanSubmissionGetDropdownListClient dropdownListClient;
+    static final String DROPDOWN_RESIDENT_TYPE = "RESIDENT_TYP";
+    static final String DROPDOWN_SALUTATION_TYPE = "SALUTATION";
 
-    public ResponseIndividual updateCustomerInfo(PersonalDetailSaveInfoRequest request) throws ServiceException, TMBCommonException, JsonProcessingException, RemoteException {
+    public PersonalDetailResponse updateCustomerInfo(PersonalDetailSaveInfoRequest request) throws ServiceException, TMBCommonException, JsonProcessingException, RemoteException {
         RequestIndividual responseIndividual = new RequestIndividual();
 
         Individual individual = getCustomerInfo(request.getCaId());
 
         Body body = new Body();
 
-        individual.setAddresses(prepareAddress(individual,request.getAddress()).getAddresses());
+        individual.setAddresses(prepareAddress(individual, request.getAddress()).getAddresses());
         individual.setPersonalInfoSavedFlag("Y");
         individual.setNationality(request.getNationality());
         individual.setMobileNo(request.getMobileNo());
@@ -54,14 +57,52 @@ public class PersonalDetailSaveInfoService {
 
         body.setIndividual(individual);
         responseIndividual.setBody(body);
-        return saveCustomer(responseIndividual.getBody().getIndividual());
+        return saveCustomer(request.getCaId(), responseIndividual.getBody().getIndividual(), request);
     }
 
-    private ResponseIndividual saveCustomer(Individual individual) throws ServiceException, TMBCommonException, JsonProcessingException {
+    private PersonalDetailResponse saveCustomer(Long caId, Individual individual, PersonalDetailSaveInfoRequest request) throws ServiceException, TMBCommonException, JsonProcessingException, RemoteException {
         try {
             ResponseIndividual response = updateCustomerClient.updateCustomerInfo(individual);
+            PersonalDetailResponse detailResponse = new PersonalDetailResponse();
+            Address address = new Address();
+
             if (response != null) {
-                return response;
+                Individual responseIndividual = getCustomerInfo(caId);
+
+                Optional<com.tmb.common.model.legacy.rsl.common.ob.address.Address> responseAddress = Arrays.stream(responseIndividual.getAddresses()).filter(x -> x.getAddrTypCode().equals("R")).findFirst();
+                if (responseAddress.isPresent()) {
+                    com.tmb.common.model.legacy.rsl.common.ob.address.Address personalAddress = responseAddress.get();
+                    address.setId(personalAddress.getId());
+                    address.setNo(personalAddress.getAddress());
+                    address.setAmphur(personalAddress.getAmphur());
+                    address.setTumbol(personalAddress.getTumbol());
+                    address.setProvince(personalAddress.getProvince());
+                    address.setRoad(personalAddress.getRoad());
+                    address.setStreetName(personalAddress.getStreetName());
+                    address.setPostalCode(personalAddress.getPostalCode());
+                    address.setMoo(personalAddress.getMoo());
+                    address.setFloor(personalAddress.getFloor());
+                    address.setCountry(personalAddress.getCountry());
+                    address.setBuildingName(personalAddress.getBuildingName());
+                }
+
+                detailResponse.setAddress(address);
+                detailResponse.setCitizenId(responseIndividual.getIdNo1());
+                detailResponse.setThaiName(responseIndividual.getThaiName());
+                detailResponse.setThaiSurname(responseIndividual.getThaiSurName());
+                detailResponse.setNationality(responseIndividual.getNationality());
+                detailResponse.setMobileNo(responseIndividual.getMobileNo());
+                detailResponse.setEmail(responseIndividual.getEmail());
+                detailResponse.setBirthDate(responseIndividual.getBirthDate());
+                detailResponse.setEngName(responseIndividual.getNameLine2());
+                detailResponse.setEngSurname(responseIndividual.getNameLine1());
+                detailResponse.setExpiryDate(responseIndividual.getExpiryDate());
+                detailResponse.setIdIssueCtry1(responseIndividual.getIdIssueCtry1());
+                detailResponse.setThaiSalutationCode(prepareDropDown(DROPDOWN_SALUTATION_TYPE,request.getThaiSalutationCode()));
+                detailResponse.setResidentFlag(prepareDropDown(DROPDOWN_RESIDENT_TYPE,request.getResidentFlag()));
+
+
+                return detailResponse;
             } else {
                 throw new TMBCommonException(ResponseCode.FAILED.getCode(),
                         ResponseCode.FAILED.getDesc(),
@@ -90,7 +131,6 @@ public class PersonalDetailSaveInfoService {
         }
 
     }
-
 
 
     private Individual prepareAddress(Individual individual, Address address) {
@@ -127,4 +167,35 @@ public class PersonalDetailSaveInfoService {
         return individual;
     }
 
+    private CommonCodeEntry[] getDropdownList(String categoryCode) throws ServiceException, TMBCommonException, JsonProcessingException {
+        ResponseDropdown getDropdownListResp = dropdownListClient.getDropDownListByCode(categoryCode);
+        return getDropdownListResp.getBody().getCommonCodeEntries();
+    }
+
+    private List<DropDown> getDropDown(String type) throws ServiceException, TMBCommonException, JsonProcessingException {
+        List<DropDown> dropDowns = new ArrayList<>();
+        CommonCodeEntry[] entries = getDropdownList(type);
+        for (CommonCodeEntry e : entries) {
+            DropDown dropDown = new DropDown();
+            dropDown.setEntryId(e.getEntryID());
+            dropDown.setEntryCode(e.getEntryCode());
+            dropDown.setEntryNameEng(e.getEntryName());
+            dropDown.setEntryNameTh(e.getEntryName2());
+            dropDown.setEntrySource(e.getEntrySource());
+            dropDowns.add(dropDown);
+        }
+        return dropDowns;
+    }
+
+    private List<DropDown> prepareDropDown(String type, String flag) throws ServiceException, TMBCommonException, JsonProcessingException {
+        List<DropDown> commonCodeEntry = getDropDown(type);
+        List<DropDown> filterList = new ArrayList<>();
+        commonCodeEntry.forEach(e -> {
+            if (flag.equals(e.getEntryCode())) {
+                filterList.add(e);
+            }
+        });
+
+        return filterList;
+    }
 }

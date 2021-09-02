102ef742cd4cac278ade013eed0010e0bc3e3466
phongsathon.k <phongsathon.k@tcs.com>
2021-09-02 16:34:38 +0700

    [NEW] add service fetch loan staging bar

--- src/main/java/com/tmb/oneapp/lendingservice/client/CommonServiceFeignClient.java
+++ src/main/java/com/tmb/oneapp/lendingservice/client/CommonServiceFeignClient.java
@@ -2,6 +2,7 @@ package com.tmb.oneapp.lendingservice.client;
 
 import com.tmb.common.model.LovMaster;
 import com.tmb.common.model.TmbOneServiceResponse;
+import com.tmb.common.model.loan.stagingbar.LoanStagingbar;
 import com.tmb.oneapp.lendingservice.constant.LendingServiceConstant;
 import com.tmb.oneapp.lendingservice.model.ProductConfig;
 import com.tmb.oneapp.lendingservice.model.RslMessage;
@@ -9,6 +10,8 @@ import com.tmb.oneapp.lendingservice.model.config.LendingModuleConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,4 +38,10 @@ public interface CommonServiceFeignClient {
             @RequestHeader(value = LendingServiceConstant.HEADER_X_CRMID) String crmId,
             @RequestParam(value = "type") String searchType,
             @RequestParam(value = "lang") String defaultLang);
+    
+    @PostMapping(value = "/apis/common/fetch/staging-bar")
+    TmbOneServiceResponse<LoanStagingbar> fetchLoanStagingBar(
+            @RequestHeader(value = LendingServiceConstant.HEADER_CORRELATION_ID) String correlationId,
+            @RequestHeader(value = LendingServiceConstant.HEADER_X_CRMID) String crmId,
+            @RequestBody(required = true) LoanStagingbar request);
 }

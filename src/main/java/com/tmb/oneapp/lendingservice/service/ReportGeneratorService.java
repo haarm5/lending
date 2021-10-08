eb9df9dabd2e32642414d2eed46c84438755d827
Nok <omerta@odds.team>
2021-10-08 14:37:32 +0700

    fixed smell

--- src/main/java/com/tmb/oneapp/lendingservice/service/ReportGeneratorService.java
+++ src/main/java/com/tmb/oneapp/lendingservice/service/ReportGeneratorService.java
@@ -1,6 +1,5 @@
 package com.tmb.oneapp.lendingservice.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tmb.common.exception.model.TMBCommonException;

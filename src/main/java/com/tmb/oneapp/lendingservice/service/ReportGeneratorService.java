646dcdc0551e6a87dbe18c6715ffdf5911701e4e
kamonwan <kamonwan294@gmail.com>
2021-10-11 16:03:53 +0700

    update subTitle flag

--- src/main/java/com/tmb/oneapp/lendingservice/service/ReportGeneratorService.java
+++ src/main/java/com/tmb/oneapp/lendingservice/service/ReportGeneratorService.java
@@ -110,6 +110,7 @@ public class ReportGeneratorService {
         LoanSubmissionGetApplicationInfoRequest rslRequest = new LoanSubmissionGetApplicationInfoRequest();
         LoanSubmissionInstantLoanSubmitApplicationRequest applicationRequest = new LoanSubmissionInstantLoanSubmitApplicationRequest();
         applicationRequest.setCaId(request.getCaId());
+        applicationRequest.setSubmittedFlag(applicationRequest.getSubmittedFlag());
         rslRequest.setCaId(request.getCaId());
         rslService.submitInstantLoanApplication(applicationRequest);
         ResponseApplication applicationInfo = rslService.getLoanSubmissionApplicationInfo(rslRequest);

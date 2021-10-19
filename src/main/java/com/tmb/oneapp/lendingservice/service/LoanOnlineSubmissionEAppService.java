7749847df2329c1b896b7314de119c6c382b1ba4
Nok <omerta@odds.team>
2021-10-19 15:16:55 +0700

    fixed issue ncb consent date on e-app is invalid

--- src/main/java/com/tmb/oneapp/lendingservice/service/LoanOnlineSubmissionEAppService.java
+++ src/main/java/com/tmb/oneapp/lendingservice/service/LoanOnlineSubmissionEAppService.java
@@ -79,7 +79,7 @@ public class LoanOnlineSubmissionEAppService {
         response.setWaiveDoc(application.getNatureOfRequest().equals("04") || application.getNatureOfRequest().equals("12"));
         response.setAcceptBy("Access Pin");
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        Date date = sdf.parse(application.getApplicationDate());
+        Date date = sdf.parse(application.getNcbConsentDate());
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         response.setAcceptDate(cal);
